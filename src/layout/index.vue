9a4a07975c84e674f8e17d03f2cbf389a13cd104
DungNguyen0209 <80510839+DungNguyen0209@users.noreply.github.com>
2024-10-30 00:28:11 +0700

    Init

---
+++ src/layout/index.vue
@@ -0,0 +1,63 @@
+<script setup>
+const usedApplicationNotificationProvide = useApplicationNotificationProvide()
+const usedApplicationAddressProvide = useApplicationAddressProvide()
+const usedAuthLoginRefreshProvide = useAuthLoginRefreshProvide()
+</script>
+
+<script>
+import { AppMain, Navbar, Sidebar, TagsView } from './components'
+import ResizeMixin from './mixin/ResizeHandler'
+import { mapState } from 'vuex'
+
+export default {
+  name: 'Layout',
+  components: {
+    AppMain,
+    Navbar,
+    Sidebar,
+    TagsView,
+  },
+  mixins: [ResizeMixin],
+  computed: {
+    ...mapState({
+      sidebar: state => state.app.sidebar,
+      device: state => state.app.device,
+      needTagsView: state => state.settings.tagsView,
+      fixedHeader: state => state.settings.fixedHeader,
+    }),
+    classObj() {
+      return {
+        hideSidebar: !this.sidebar.opened,
+        openSidebar: this.sidebar.opened,
+        withoutAnimation: this.sidebar.withoutAnimation,
+        mobile: this.device === 'mobile',
+      }
+    },
+  },
+  methods: {
+    handleClickOutside() {
+      this.$store.dispatch('app/closeSideBar', { withoutAnimation: false })
+    },
+  },
+}
+</script>
+
+<template>
+  <div :class="classObj" class="app-wrapper">
+    <AppLoading />
+    <div v-if="device === 'mobile' && sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
+    <Sidebar class="sidebar-container" />
+    <div :class="{ hasTagsView: needTagsView }" class="main-container">
+      <div :class="{ 'fixed-header': fixedHeader }">
+        <Navbar />
+        <TagsView v-if="needTagsView" />
+      </div>
+      <AppMain />
+      <footer>
+        <div class="copy-right">
+          Yoong @ All Rights Reserved. Stonehub CMS v4.3.1
+        </div>
+      </footer>
+    </div>
+  </div>
+</template>
